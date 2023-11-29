f89792d61fd8ca0f2566a2fd98bcb03b73f7c3f8
jduncan017 <emailjoshduncan@gmail.com>
2023-11-29 01:35:10 -0700

    untest

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const userRouter = require("./userRoutes");
 const { NotFoundError } = require("../middlewares/error-handler/error-handler");
 
 router.use("/items", itemRouter);
-router.use("/user", userRouter);
+router.use("/users", userRouter);
 
 router.use((req, res, next) => {
   next(new NotFoundError("Router resource not found."));
